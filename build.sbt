b68b7460f87547161058886e02074c551dae5755
quentin <quentin.ambard@datastax.com>
2018-04-08 15:25:26 +0100

    update

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+import sbtassembly.PathList
+
 name := "jvm_bench"
 
 version := "0.1"
@@ -6,3 +8,31 @@ scalaVersion := "2.12.4"
 
 libraryDependencies += "io.gatling" % "gatling-core" % "2.2.3"
 libraryDependencies += "io.gatling" % "gatling-http" % "2.2.3"
+libraryDependencies += "com.google.guava" % "guava" % "24.1-jre"
+
+
+
+// resolvers += "Job Server Bintray" at "https://dl.bintray.com/spark-jobserver/maven"
+
+assemblyMergeStrategy in assembly := {
+  case PathList("javax", "servlet", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "activation", xs @ _*) => MergeStrategy.last
+  case PathList("org", "apache", xs @ _*) => MergeStrategy.last
+  case PathList("com", "google", xs @ _*) => MergeStrategy.last
+  case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last
+  case PathList("com", "codahale", xs @ _*) => MergeStrategy.last
+  case PathList("com", "yammer", xs @ _*) => MergeStrategy.last
+  case "about.html" => MergeStrategy.rename
+  case "META-INF/ECLIPSEF.RSA" => MergeStrategy.last
+  case "META-INF/mailcap" => MergeStrategy.last
+  case "META-INF/mimetypes.default" => MergeStrategy.last
+  case "plugin.properties" => MergeStrategy.last
+  case "log4j.properties" => MergeStrategy.last
+  case x if x.endsWith("io.netty.versions.properties") => MergeStrategy.first
+  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
+  case x => MergeStrategy.first
+  //  case x =>
+  //    val oldStrategy = (assemblyMergeStrategy in assembly).value
+  //    oldStrategy(x)
+}
+
--- src/main/scala/Test.java
+++ src/main/scala/Test.java
@@ -1,17 +1,95 @@
-import com.datastax.driver.core.NettySSLOptions;
-import com.datastax.driver.core.SSLOptions;
-import io.netty.handler.ssl.ApplicationProtocolConfig;
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.SslProvider;
+import com.google.common.base.Joiner;
+import com.google.common.base.Splitter;
+import scala.collection.mutable.StringBuilder;
 
-import javax.net.ssl.TrustManagerFactory;
-import java.io.*;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 
 public class Test {
 
 
+    public static void main(String[] args) throws IOException {
+        String logFile = args.length>0 ? args[0] : "/home/quentin/Downloads/jvm_bench/test-32-31GB-300ms-rs-32-1522861280706/simulation.log";
+        String outputFile = args.length>1 ? args[1] : "/home/quentin/Downloads/jvm_bench/test-32-31GB-300ms-rs-32-1522861280706/result.csv";
+        System.out.println("logFile="+logFile);
+        System.out.println("outputFile="+outputFile);
+        String testName = logFile.substring(0,logFile.lastIndexOf("/"));
+        testName = logFile.substring(testName.lastIndexOf("/")+1, testName.length());
+
+        int control = 0;
+        long t1 = System.currentTimeMillis();
+//        List<Long> timeOK = new ArrayList<>(48 * 1400);
+//        List<Long> timeKO = new ArrayList<>(48 * 1400);
+        List<Long> timeALL = new ArrayList<>(48 * 1400);
+        Splitter splitter = Splitter.on("\t");
+        try (BufferedReader br = new BufferedReader(new FileReader(logFile))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+//                if (control % 10000000 == 0) {
+//                    System.out.println(control);
+//                }
+//                control++;
+                if (line.startsWith("RE")) { //REQUEST
+                    Iterator<String> cols = splitter.split(line).iterator(); //15% faster than a string.split?
+                    Long start = null;
+                    Long end = null;
+                    for(int i =0;i<=6;i++){
+                        String cell = cols.next();
+                        if(i==5) start = Long.valueOf(cell);
+                        else if(i==6) end = Long.valueOf(cell);
+                    }
+                    timeALL.add(end-start);
+                }
+            }
+        }
+        long t2 = System.currentTimeMillis();
+        System.out.println("took "+(t2-t1)+". sorting...");
+        Collections.sort(timeALL);
+        System.out.println("sorting ok");
+
+        Map<Integer, Long> percentiles = new LinkedHashMap<>();
+        for(int i = 1; i<=10000;i++){
+            percentiles.put(i, getPercentile(timeALL, i));
+        }
+        String csvLine = testName + "," + Joiner.on(",").join(percentiles.values());
+        System.out.println(csvLine);
+        Files.write(Paths.get(outputFile), csvLine.getBytes(), StandardOpenOption.APPEND);
+
+//        for(Map.Entry<Integer, Long> e : percentiles.entrySet()){
+//            sb.append(e.getValue()+",");
+//            System.out.println(e.getKey()+"=>"+e.getValue());
+//        }
+    }
+
+    public static long getPercentile(List<Long> latencies, double percentile) {
+        int index = (int) Math.ceil((percentile / 10000.0) * (double) latencies.size());
+        return latencies.get(index - 1);
+    }
 }
+
+// Guava percentile lib is dead slow...
+//        System.out.println("Computing quantiles1");
+//        Map<Integer, Double> results1 = Quantiles.scale(1000).indexes(500).compute(timeOK);
+
+//        System.out.println("Computing quantiles"+results1.size());
+//        ArrayList<Integer> percentiles = new ArrayList();
+//        for(int i = 500; i<799;i+=100){
+//            percentiles.add(i);
+//        }
+//        for(int i = 800; i<989;i+=10){
+//            percentiles.add(i);
+//        }
+//        for(int i = 990; i<=1000;i++){
+//            percentiles.add(i);
+//        }
+//        Map<Integer, Double> results = Quantiles.scale(1000).indexes(percentiles).compute(timeOK);
+//        ArrayList<Integer> keys = new ArrayList<Integer>(results.keySet());
+//        Collections.sort(keys);
+//        for(int i : keys){
+//            System.out.println(i+" -> "+results.get(i));
+//        }
